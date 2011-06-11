510fa1fb43365f28ae52d1cb2802434ef934f757
Ben Walsham <ben.walsham@meltingwaldo.com>
2011-06-11 14:41:21 +0100

    more tests

--- test/unit/bill_test.rb
+++ test/unit/bill_test.rb
@@ -6,11 +6,11 @@ class BillTest < ActiveSupport::TestCase
     @adam = Fabricate(:adam)
     @ben = Fabricate(:ben)
     @charlie = Fabricate(:charlie)
-    # @dan = Fabricate(:dan)
+    @dan = Fabricate(:dan)
     # @ed = Fabricate(:ed)
   end
   
-  test "bill payed by one on behalf of two" do
+  test "bill payed by one on behalf of himself plus one" do
     bill = Fabricate(:bill, :people => [@adam, @ben])
     bill.expenses << Fabricate.build(:expense, 
       :amount => 2,
@@ -24,6 +24,27 @@ class BillTest < ActiveSupport::TestCase
       :creditor => @adam,
       :debitor => @ben
     )
+  end
+  
+  test "bill payed by one on behalf of two" do
+    bill = Fabricate(:bill, :people => [@adam, @ben, @charlie])
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 2,
+      :creditors => [@adam], 
+      :debitors => [@charlie, @ben]
+    )
+    bill.save
+    assert bill.payments.length == 2   
+    assert bill.payments.include? Fabricate.build(:payment,
+      :amount => 1,
+      :creditor => @adam,
+      :debitor => @ben
+    )
+    assert bill.payments.include? Fabricate.build(:payment,
+      :amount => 1,
+      :creditor => @adam,
+      :debitor => @charlie
+    )    
   end
   
   test "bill payed by one on behalf of three" do
@@ -43,5 +64,80 @@ class BillTest < ActiveSupport::TestCase
       :amount => 1, 
       :creditor => @adam, 
       :debitor => @charlie)
+  end
+  
+  test "bill payed by two on behalf of three" do
+    bill = Fabricate(:bill, :people => [@adam, @ben, @charlie])
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 3,
+      :creditors => [@adam, @ben], 
+      :debitors => [@adam, @ben, @charlie]
+    )
+    bill.save
+    assert bill.payments.length == 2
+    assert bill.payments.include? Fabricate.build(:payment, 
+      :amount => 0.5, 
+      :creditor => @adam, 
+      :debitor => @charlie)
+    assert bill.payments.include? Fabricate.build(:payment, 
+      :amount => 0.5, 
+      :creditor => @ben, 
+      :debitor => @charlie)
+  end
+  
+  test "two bills payed by one on behalf of two" do
+    bill = Fabricate(:bill, :people => [@adam, @ben])
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 1,
+      :creditors => [@adam], 
+      :debitors => [@adam, @ben]
+    )
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 1,
+      :creditors => [@adam], 
+      :debitors => [@adam, @ben]
+    )
+    bill.save
+    assert bill.payments.length == 1
+    assert bill.payments.include? Fabricate.build(:payment, 
+      :amount => 1, 
+      :creditor => @adam, 
+      :debitor => @ben)
+  end
+  
+  test "two bills payed by two on behalf each other balances out" do
+    bill = Fabricate(:bill, :people => [@adam, @ben])
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 1,
+      :creditors => [@adam], 
+      :debitors => [@adam, @ben]
+    )
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 1,
+      :creditors => [@ben], 
+      :debitors => [@adam, @ben]
+    )
+    bill.save
+    assert bill.payments.length == 0
+  end
+  
+  test "two bills payed by two on behalf of each other with different amounts" do
+    bill = Fabricate(:bill, :people => [@adam, @ben])
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 3,
+      :creditors => [@adam], 
+      :debitors => [@adam, @ben]
+    )
+    bill.expenses << Fabricate.build(:expense, 
+      :amount => 1,
+      :creditors => [@ben], 
+      :debitors => [@adam, @ben]
+    )
+    bill.save
+    assert bill.payments.length == 1
+    assert bill.payments.include? Fabricate.build(:payment, 
+      :amount => 1, 
+      :creditor => @adam, 
+      :debitor => @ben)
   end
 end
